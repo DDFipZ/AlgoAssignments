54bedd5febec9805ca754e3a57494eec72a1fd4d
j-winkel <43139461+j-winkel@users.noreply.github.com>
2020-04-15 18:24:38 +0200

    Fixed

--- Algorithm Part 1/PQHeap.py
+++ Algorithm Part 1/PQHeap.py
@@ -6,6 +6,7 @@ Alban Dalifi                aldal18
 
 Alban er ikke tilmeldt kursus endnu, da han venter svar fra studienævnet
 """
+import math
 
 #Heap Sort
 #A is a list
@@ -14,40 +15,52 @@ Alban er ikke tilmeldt kursus endnu, da han venter svar fra studienævnet
 #Inserts key value into A list 
 
 def Parent(i):
-    return (i - 1) // 2
+    return math.floor((i-1/2))
+
+def Left(i):
+    return (2 * i + 1 )
+
+def Right(i):
+    return (2 * i + 2 )
 
 
 def insert(A, key):
     A.append(key)
-    i = len(A)
-    while i > 1 and A[Parent(i)] < A[i]:
+    i = len(A) - 1
+
+    while i > 0 and A[Parent(i)] > A[i]:
         A[i], A[Parent(i)] = A[Parent(i)], A[i]
         i = Parent(i)
 
 
 #Extracts the minumum value in heap
 def extractMin(A):
-    if len(A) < 1:
-        return "Heap Underflow"
+    #lenght = len(A)
+    # if len(A) < 1:
+    #     return "Heap Underflow"
+        
     min = A[0]
-    A[0] = A[len(A) - 1]
-    low_heapify(A, 1)
-    return max
+    A.pop(0)
+    #A[0] = A[A[lenght]]
+    #A[lenght] = [lenght - 1]
+    low_heapify(A, 0)
+    return min
 
 
 #Heapifies by lower integers
 def low_heapify(A, i):
-    l = 2 * i + 1
-    r = 2 * i + 2
-    length = len(A)
-    smallest = None
+    l = Left(i)
+    r = Right(i)
+    length = len(A) - 1
+    smallest = i
 
-    if l <= A[length - 1] and A[l] < A[i]:
+    if l <= length and A[l] < A[i]:
         smallest = l
-    else:
-        smallest = i
-    if r <= A[length - 1] and A[l] < A[smallest]:
+
+
+    if r <= length and A[r] < A[smallest]:
         smallest = r
+
     if smallest != i:
         A[i], A[smallest] = A[smallest], A[i]
         low_heapify(A, smallest)
