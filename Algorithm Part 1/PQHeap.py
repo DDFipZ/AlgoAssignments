70ade784f8aa5a08655afea9ea1b5927a36744f1
Marcus Pedersen <marcus2860@gmail.com>
2020-04-15 13:35:06 +0200

    algo assignment 1 almost done

--- Algorithm Part 1/PQHeap.py
+++ Algorithm Part 1/PQHeap.py
@@ -12,43 +12,42 @@ Alban er ikke tilmeldt kursus endnu, da han venter svar fra studienævnet
 #e is a number to be inserted into the A list
 
 #Inserts key value into A list 
+
+def Parent(i):
+    return (i - 1) // 2
+
+
 def insert(A, key):
-    A.append(int(key))
-    heapSort(A)
+    A.append(key)
+    i = len(A)
+    while i > 1 and A[Parent(i)] < A[i]:
+        A[i], A[Parent(i)] = A[Parent(i)], A[i]
+        i = Parent(i)
+
 
 #Extracts the minumum value in heap
 def extractMin(A):
-    length = len(A)
+    if len(A) < 1:
+        return "Heap Underflow"
     min = A[0]
-    A[0] = A[length - 1]
-    A.pop()
-    heapSort(A)
-    return min
+    A[0] = A[len(A) - 1]
+    low_heapify(A, 1)
+    return max
+
 
 #Heapifies by lower integers
-def low_heapify(A, n, i):
-    smallest = i
-    #left child 
+def low_heapify(A, i):
     l = 2 * i + 1
-    #Right Child
     r = 2 * i + 2
+    length = len(A)
+    smallest = None
 
-    #Checks left child
-    if l < n and A[smallest] > A[l]:
+    if l <= A[length - 1] and A[l] < A[i]:
         smallest = l
-
-    #Checks Right Child
-    if r < n and A[smallest] > A[r]:
+    else:
+        smallest = i
+    if r <= A[length - 1] and A[l] < A[smallest]:
         smallest = r
-
-    #Checks if root is smallest, if not, switcharoo
     if smallest != i:
         A[i], A[smallest] = A[smallest], A[i]
-        #Heapifies
-        low_heapify(A, n, smallest)
-
-#Performs the heapsort
-def heapSort(A):
-    n = len(A)
-    for i in range(int(n/2) -1, -1, -1):
-        low_heapify(A, n, i)
+        low_heapify(A, smallest)
